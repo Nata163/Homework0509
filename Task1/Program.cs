362f4412380303bb1f6810c41ce8aa1feec6e0a9
Nata <nata.9688@mail.ru>
2022-09-05 10:15:25 +0400

    Add task1 max number

---
+++ Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+int max = number1;
+if (number2 > number1)
+{
+    max = number2;
+    Console.Write(number2);
+}
+else
+{
+    Console.Write(number1);
+}
