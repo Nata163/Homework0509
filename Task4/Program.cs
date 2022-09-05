507d6a579e56e3b060ff1e7e93c7a44ea865e882
Nata <nata.9688@mail.ru>
2022-09-05 11:20:38 +0400

    Add task4 even number up to

---
+++ Task4/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int x = 1;
+int count = 0;
+while (x < number)
+{
+    if (x % 2==0)
+    {
+        count = count+1;
+        Console.WriteLine(x);
+    }
+    else
+    {
+        count = count;
+    }
+x++;
+}
